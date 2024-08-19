8bdb77a83728446cbefcc61663861a76ddbf1c89
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-08-19 16:52:53 +0700

    Channel with a User

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -1,14 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React from 'react'
+import { useChatContext } from 'stream-chat-expo'
+import { useAuth } from '../providers/AuthProvider';
+import { router } from 'expo-router';
 
 interface User {
-    full_name: string
+    full_name: string,
+    id: any,
 }
 
 export default function UserListItem({ user }: { user: User }) {
+
+    const { client } = useChatContext();
+
+    const { user: me } = useAuth();
+
+    const onPress = async () => {
+        const channel = client.channel("messaging", {
+            members: [me?.id, user.id]
+        });
+        await channel.create();
+        router.replace(`/(home)/channel/${channel.cid}`);
+    }
+
     return (
-        <View style={{ padding: 15, backgroundColor: "white" }} >
+        <Pressable onPress={onPress} style={{ padding: 15, backgroundColor: "white" }} >
             <Text style={{ fontWeight: "600" }} >{user.full_name}</Text>
-        </View>
+        </Pressable>
     )
 }
